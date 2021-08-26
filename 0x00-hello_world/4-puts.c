8484775da0427d18e600c0c91e37880245fb3216
CharaD7 <jijakahn6@gmail.com>
2021-08-26 18:23:49 +0000

    Print a string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,9 @@
 /* This is a basic C program that prints a 
  * specific string using puts function
  */
-int main(void){
+
+int main(void)
+{
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
