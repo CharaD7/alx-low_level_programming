3f83f182946b4d049f678e96f60fcef219a46fa7
CharaD7 <jijakahn6@gmail.com>
2021-08-30 07:25:49 +0000

    Print numbers from 0 - 9, each on a new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Desccription: Print 0 to 9, each on a new line
+ * Return: (0)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+	printf("%d\n", n);
+}
+
+return (0);
+}
