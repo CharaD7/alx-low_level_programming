da5d2cd3d997103ee3d2e112abf9a61f847f0412
CharaD7 <jijakahn6@gmail.com>
2021-09-01 18:40:50 +0000

    Check for absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs: checks the absolute value of an integer
+ *
+ * @n: check number
+ *
+ * Return: 0 or 1
+ */
+
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}
