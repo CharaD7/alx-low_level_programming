12ab6aec6237e2208a0f9f89b81e62a2296e70f6
CharaD7 <jijakahn6@gmail.com>
2021-09-01 23:14:00 +0000

    Last Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-
 /**
- * main - prints fibonacci numder to 98
- *
- * Return: Always 0
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	int n1 = 1;
-	int n2 = 2;
-	int y, z;
+	int i = 0;
+	unsigned long int a = 0, b = 1, next = 0;
 
-	for (y = 1; y <= 98; y++)
+	while (i < 98)
 	{
-		printf("%d ,", n1);
-		z = n2 + n1;
-		n1 = n2;
-		n2 = z;
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 97)
+			printf(", ");
+		i++;
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
