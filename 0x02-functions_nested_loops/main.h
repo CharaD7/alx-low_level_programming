d9841fadbf5fbb7cf34d66dcbc159fa0918c217e
CharaD7 <jijakahn6@gmail.com>
2021-09-01 17:10:19 +0000

    Added main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void print_to_98(int n);
+void times_table(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _putchar(char c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+int _islower(int c);
+
+#endif
