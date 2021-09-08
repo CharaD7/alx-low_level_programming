12454aab6d3e1088b0744057db9df87ee1e192a2
CharaD7 <jijakahn6@gmail.com>
2021-09-08 18:04:05 +0000

    Copy string pointed to source

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Copy string pointed to be c
+ *
+ * @dest: Pointer destination
+ *
+ * @src: Poniter to source
+ *
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do
+	{
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
