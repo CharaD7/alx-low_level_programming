af3fe95e89d480ae8634ef2d1f794874bf2b8a68
CharaD7 <jijakahn6@gmail.com>
2021-09-18 18:18:56 +0000

    Compress two strings and return 1 if identical, 0 if not

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,104 +1,69 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * checkLast - Checks the last character of s2 when s1 ends
- *
- * @s: string
- *
- * @i: int
- *
- * Return: 0 or 1
- */
+* substring_match - check if a substring after wildcard matches s1
+*
+* @s1: one string
+*
+* @s2: one string
+*
+* @after_wldcd: placeholder for position right after wildcard
+*
+* Return: 1 if matched, 0 if not
+*/
 
-int checkLast(char *s, int i)
+int substring_match(char *s1, char *s2, char *after_wldcd)
 {
-
-	if (s[i] == '*')
-	{
-		return (checkLast(s, i + 1));
-	}
-
-	else if (s[i] == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
 	}
-
-	return (0);
-
+	if (*s1 == '\0' && *s2 == '*')
+	{
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	}
+	if (*s1 == '\0' && *s2 != '\0')
+	{
+		return (0);
+	}
+	if (*s2 == '*')
+	{
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	}
+	if (*s1 == *s2)
+	{
+		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+	}
+	else
+	{
+		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+	}
 }
 
 /**
- * checker - helper
- *
- * @s1: string
- *
- * @s2: string
- *
- * @a: int
- *
- * @b: int
- *
- * @wildUsed: int
- *
- * Return: Always 0.
- */
-
-int checker(char *s1, char *s2, int a, int b, int wildUsed)
-{
-	if (s1[a] != '\0')
-	{
-		if (s2[b] == '\0')
-		{
-			return (0);
-		}
-		else if (s2[b] == '*')
-		{
-			if (s2[b + 1] == '*')
-			{
-				return (checker(s1, s2, a, b + 1, b));
-			}
-			else if (s2[b + 1] == s1[a])
-			{
-				return (checker(s1, s2, a, b + 1, b));
-			}
-			else if (s1[a + 1] != s2[b + 1])
-			{
-				return (checker(s1, s2, a + 1, b, b));
-			}
-			else if (s1[a + 1] == s2[b + 1])
-			{
-				return (checker(s1, s2, a + 1, b + 1, b));
-			}
-		}
-		else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
-		{
-			return (checker(s1, s2, a + 1, b + 1, wildUsed));
-		}
-		if (wildUsed == -1)
-		{
-			return (0);
-		}
-		return (checker(s1, s2, a, wildUsed, wildUsed));
-	}
-	if (s2[b] != '\0')
-	{
-		return (checkLast(s2, b));
-	}
-	return (1);
-}
-
-/**
- * wildcmp - Checks the code
- *
- * @s1: string
- *
- * @s2: string
- *
- * Return: Always 0.
- */
+* wildcmp - compare if string with wildcard mattches
+*
+* @s1: one string
+*
+* @s2: one string
+*
+* Return: 1 if matched, 0 if not
+*/
 
 int wildcmp(char *s1, char *s2)
 {
-	return (checker(s1, s2, 0, 0, -1));
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else if (*s2 == '*')
+	{
+		return (substring_match(s1, (s2 + 1), (s2 + 1)));
+	}
+	else
+	{
+		return (0);
+	}
 }
