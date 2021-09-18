1872e45c8951169e5e67577c6818ce28771ab2b6
CharaD7 <jijakahn6@gmail.com>
2021-09-18 18:25:15 +0000

    Reverted changes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,10 +13,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0 ; i++;)
+	for (i = 0; n > 0 ; i++, n--;)
 	{
 		dest[i] = src[i];
-		n--;
 	}
 
 	return (dest);
