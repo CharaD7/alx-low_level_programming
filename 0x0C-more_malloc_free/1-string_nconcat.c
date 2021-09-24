e7178153ca9051ff661a1816547a89699cd08508
CharaD7 <jijakahn6@gmail.com>
2021-09-24 23:05:24 +0000

    Concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * string_nconcat - Concatenate two strings
@@ -17,30 +18,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s1s2;
 	unsigned int s1_size, s2_size, s1_count, s2_count;
 
+	s1_size = strlen(s1);
+	s2_size = strlen(s2);
+
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
 
-	for (s1_size = 0; s1[s1_size] != '\0'; s1_size++)
-		;
-
-	for (s2_size = 0; s2[s2_size] != '\0'; s2_size++)
-		;
-
 	if (n < s2_size)
 		s2_size = n;
 
-	s1s2 = malloc(sizeof(char) * (s1_size + s2_size + 1));
+	s1s2 = malloc(sizeof(s1s2) * (s1_size + n + 1));
 
 	if (s1s2 == NULL)
 		return (NULL);
 
-	for (s1_count = 0; s1_count < s1_size; s1_count++)
+	for (s1_count = 0; s1[s1_count] != '\0'; s1_count++)
 		s1s2[s1_count] = s1[s1_count];
 
-	for (s2_count = 0; s2_count < s2_size; s2_count++, s1_count++)
+	for (s2_count = 0; s2_count < n && s2[s2_count] != '\0'; s2_count++, s1_count++)
 		s1s2[s1_count] = s2[s2_count];
 
 	s1s2[s1_count] = '\0';
